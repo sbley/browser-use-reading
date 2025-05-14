f3644643305d4525347d8962120902a4dfa755a6
Stefan Bley <stefan.bley@zeiss.com>
2025-05-14 14:23:30 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,56 @@
+import asyncio
+
+from browser_use.browser.browser import Browser
+from browser_use.browser.context import BrowserContextConfig, BrowserContext
+from dotenv import load_dotenv
+
+load_dotenv()
+from browser_use import Agent
+from langchain_openai import AzureChatOpenAI
+
+task = """
+   ### Prompt for Read Assignment Confirmations
+   
+**Objective:**
+Visit [Read Assignments](https://zdi-wiki.zeiss.com/users/readack/view.action), identify all pending read assignments. 
+Go to each of them by visiting the linked page and confirm that the page has been read by clicking the confirm button 
+on the bottom.
+Sign in before if necessary.
+---
+
+### Step 1: Navigate to the website
+- [Read Assignments](https://zdi-wiki.zeiss.com/users/readack/view.action).
+- If not logged in already, sign in as x_username.
+---
+
+### Step 2: Confirm pending read assignments
+For each item in the table of pending read assignments
+  - Open the page
+  - Scroll to the very bottom of the page
+  - Click on the "Confirm" link
+---
+
+### Step 3: Output Summary
+- Once all pending read assignments have been confirmed, output a summary including:
+  - **List of pending read assignments**.
+  - **Info text if there weren't any pending read assignments**.
+"""
+model = AzureChatOpenAI(azure_deployment="gpt-4o-2024-08-06",
+                        api_version="2024-09-01-preview",
+                        model="gpt-4o-2024-08-06")
+
+
+async def main():
+    agent = Agent(
+        task=task,
+        llm=model,
+        sensitive_data={"x_username": "stefan.bley@zeiss.com"},
+        browser_context=BrowserContext(browser=Browser(), config=BrowserContextConfig(
+            allowed_domains=['microsoftonline.com', 'zeiss.com'])),
+        # Workaround for https://github.com/browser-use/browser-use/issues/1578
+        tool_calling_method="function_calling"
+    )
+    await agent.run()
+
+
+asyncio.run(main())
